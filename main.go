5713be1201e79d97eae5a77f2fc5d8b250a875a5
Youssefhajjaoui <youssefhajjaoui67@gmail.com>
2024-06-03 20:07:14 +0100

    gg

--- main.go
+++ main.go
@@ -3,32 +3,42 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"text/template"
 )
 
 func toget(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
 		http.Error(w, "method faile", http.StatusMethodNotAllowed)
 	}
-	http.ServeFile(w, r, "index.html")
+	t, err := template.ParseFiles("index.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = t.Execute(w, nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
 
 func topost(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		http.Error(w, "method faile", http.StatusMethodNotAllowed)
 	}
+	t, err := template.ParseFiles("index.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 	Out := r.PostFormValue("text")
-	// t, err := template.ParseFiles("index.html")
-	// if err != nil {
-	// 	http.Error(w, err.Error(), http.StatusInternalServerError)
-	// 	return
-	// }
-	// err = t.Execute(w, Out)
-	// if err != nil {
-	// 	http.Error(w, err.Error(), http.StatusInternalServerError)
-	// 	return
-	// }
-	http.ServeFile(w, r, "index.html")
-	fmt.Fprintln(w, Out)
+	fmt.Println(Out)
+	err = t.Execute(w, Out)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	// http.ServeFile(w, r, "index.html")
 	// http.Redirect(w, r, "/", 8080)
 }
 
