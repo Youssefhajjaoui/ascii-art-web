5098b261831bf63af236c5bd04b965125799ab1e
Youssefhajjaoui <youssefhajjaoui67@gmail.com>
2024-06-03 16:38:26 +0100

    nn

--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func toget(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "method faile", http.StatusMethodNotAllowed)
+	}
+	http.ServeFile(w, r, "index.html")
+}
+
+func topost(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "method faile", http.StatusMethodNotAllowed)
+	}
+	Out := r.PostFormValue("text")
+	// t, err := template.ParseFiles("index.html")
+	// if err != nil {
+	// 	http.Error(w, err.Error(), http.StatusInternalServerError)
+	// 	return
+	// }
+	// err = t.Execute(w, Out)
+	// if err != nil {
+	// 	http.Error(w, err.Error(), http.StatusInternalServerError)
+	// 	return
+	// }
+	http.ServeFile(w, r, "index.html")
+	fmt.Fprintln(w, Out)
+	// http.Redirect(w, r, "/", 8080)
+}
 
 func main() {
-	fmt.Println("project valide inchallah with team khaliha 3ala allah")
+	http.HandleFunc("/", toget)
+	http.HandleFunc("/ascii-art", topost)
+	http.ListenAndServe(":8080", http.DefaultServeMux)
 }
