8bb0ee2e18fbaf9e64679ad474bab1a0d9edefe2
Youssefhajjaoui <youssefhajjaoui67@gmail.com>
2024-06-04 23:48:08 +0100

    last commit

--- main.go
+++ main.go
@@ -23,9 +23,6 @@ func toget(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-
-	// Create an empty struct to hold form data (optional for cleaner separation)
-	// Execute the template without initial data if it's a GET request
 	err = t.Execute(w, nil)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -35,11 +32,11 @@ func toget(w http.ResponseWriter, r *http.Request) {
 
 func topost(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/ascii-art" {
-		handleerr(w, r, http.StatusNotFound, "path incorect", http.StatusText(http.StatusNotFound))
+		handleerr(w, http.StatusNotFound, "path incorect", http.StatusText(http.StatusNotFound))
 		return
 	}
 	if r.Method != http.MethodPost {
-		handleerr(w, r, http.StatusNotFound, "path incorect", http.StatusText(http.StatusNotFound))
+		handleerr(w, http.StatusNotFound, "path incorect", http.StatusText(http.StatusNotFound))
 		return
 	}
 	t, err := template.ParseFiles("index.html")
@@ -74,20 +71,21 @@ func main() {
 	http.ListenAndServe(":8080", http.DefaultServeMux)
 }
 
-func handleerr(w http.ResponseWriter, r *http.Request, statuscode int, message string, statustext string) {
+func handleerr(w http.ResponseWriter, statuscode int, message string, statustext string) {
 	t, err := template.ParseFiles("warningmsg.html")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	type errors struct {
-		Text string
-		code int // Adjust field name as needed
+		Statustext string
+		Statuscode int // Corrected field name
 	}
 	error := errors{
-		Text: statustext,
-		code: statuscode,
+		Statustext: statustext,
+		Statuscode: statuscode, // Corrected field name
 	}
+	fmt.Println(statustext)
 	err = t.Execute(w, error)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
